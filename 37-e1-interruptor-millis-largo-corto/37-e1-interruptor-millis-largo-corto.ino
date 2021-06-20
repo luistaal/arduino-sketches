4d54bbd013cca2c4c3309ccd40580ac1d067f2e0
luistaal <luistaal@gmail.com>
2021-06-20 18:17:38 +0200

    Añadido comentarios y nueva variable
    Se añade nuevos comentarios y una nueva variable [longPressTime] para seleccionar la duración del pulsado largo.

--- 37-e1-interruptor-millis-largo-corto/37-e1-interruptor-millis-largo-corto.ino
+++ 37-e1-interruptor-millis-largo-corto/37-e1-interruptor-millis-largo-corto.ino
@@ -7,48 +7,53 @@
    Distribuida bajo licencia GNU General Public License V3
 */
 
-const uint8_t pinLed = 13;			    // declare pin 13 for led
-const uint8_t pinButton = 12;		    // declare pin 12 for button
-const uint16_t onDuraction = 100;   // duraction time activated of led
-const uint16_t offDuraction = 500;  // duraction time desactivated of led
+const uint8_t pinLed = 13;             // declare pin 13 for led
+const uint8_t pinButton = 12;		      // declare pin 12 for button
+const uint16_t onDuraction = 100;      // duraction time activated of led
+const uint16_t offDuraction = 500;     // duraction time desactivated of led
+const uint16_t longPressTime = 2500;   // long press time of button
 
-uint8_t previousButtonState = 1;	  // previous button state for push control
-unsigned long initialTime = 0;		  // control initial time of push button
-unsigned long finalTime = 0;		    // control final time of push button
-unsigned long rememberTime = 0;	    // remember time for duration led blink
-uint8_t ledState = 1;				        // led status for high or low
+uint8_t previousButtonState = 1;	      // previous button state for push control
+unsigned long initialTime = 0;		   // control initial time of push button
+unsigned long finalTime = 0;		      // control final time of push button
+unsigned long rememberTime = 0;	      // remember time for duration led blink
+uint8_t ledState = 1;				      // led status for high or low
 uint8_t blinkSwitch = 0;			      // switch for activated/desactivated led blink
 
 void setup() {
-  Serial.begin(9600);		      // define serial port 9600
-  pinMode(pinLed, OUTPUT); 	  // define pinLED as output
+  Serial.begin(9600);         // define serial port 9600
+  pinMode(pinLed, OUTPUT); 	// define pinLED as output
   pinMode(pinButton, INPUT);  // define pinButton as input
 }
 
 void loop() {
-  uint8_t buttonState = digitalRead(pinButton);
+  uint8_t buttonState = digitalRead(pinButton); // read button state
 
+  // detected in state button on actived
   if (buttonState == 0 && previousButtonState == 1) {
-    Serial.println("Pulsado");
+    Serial.println("Button pressed");
     initialTime = millis();
   }
 
+  // detected in state button on desactivated
   if (buttonState == 1 && previousButtonState == 0) {
-    Serial.println("Soltado");
+    Serial.println("Button released");
     finalTime = millis();
 
-    if ((finalTime - initialTime) >= 2500) {
-      Serial.print("Pulsacion larga: ");
+    // compare the times and detect if it was a long or short press
+    if ((finalTime - initialTime) >= longPressTime) {
+      Serial.print("Long press: ");
       Serial.println(finalTime - initialTime);
       blinkSwitch = !blinkSwitch;
     } else {
-      Serial.print("Pulsacion corta: ");
+      Serial.print("Short press: ");
       Serial.println(finalTime - initialTime);
     }
   }
 
-  previousButtonState = buttonState;
+  previousButtonState = buttonState; // save the last state of the button
 
+  // activated the flashing of the led
   if (blinkSwitch == 1) {
     if (ledState == 1) {
       if ((millis() - rememberTime) >= onDuraction) {
