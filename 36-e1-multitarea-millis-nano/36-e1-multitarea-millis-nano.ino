06ac2c6db56640c025f2b78b6da80540438f99fd
luistaal <luistaal@gmail.com>
2021-09-14 18:11:48 +0200

    Optimización del código
    Se ha mejorado en el código y añadido los comentarios.

--- 36-e1-multitarea-millis-nano/36-e1-multitarea-millis-nano.ino
+++ 36-e1-multitarea-millis-nano/36-e1-multitarea-millis-nano.ino
@@ -7,80 +7,91 @@
  * @attention Distribuida bajo licencia GNU General Public License V3
  */
 
-unsigned long currentTimeA = 0;
-unsigned long currentTimeB = 0;
-long begin, final, timeNow;
-byte buttonReading_old = 1;
+const uint8_t pinLed = A5;        // declare pin A5 for led
+const uint8_t pinLedNano = 13;    // declare pin 13 for internal led
+const uint8_t pinButton = 6;      // declare pin 6 for button
 
-byte hour = 9;
-byte minute = 59;
-byte second = 55;
-String showHour;
-String showMinute;
-String showSecond;
+unsigned long currentTimeA = 0;   // variable for current time A
+unsigned long currentTimeB = 0;   // variable for current time B
+long begin, end, timeNow;         // variable for the start and end time of the alarm, in addition to the current time
+uint8_t buttonOldReading = 1;     // variable for button old reading
 
-const byte pinLed = A5;
-const byte pinLedNano = 13;
-const byte pinButton = 6;
-
-uint8_t alarmOn[] = {10, 00, 00};
-uint8_t alarmOff[] = {10, 00, 10};
+uint8_t hour = 9;                   // clock in hours
+uint8_t minute = 59;                // clock in minutes
+uint8_t second = 55;                // clock in seconds
+uint8_t alarmOn[] = {10, 00, 00};   // clock for alarm on
+uint8_t alarmOff[] = {10, 00, 10};  // clock for alarm off
 
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(pinLed, OUTPUT);
-  pinMode(pinLedNano, OUTPUT);
-  pinMode(pinButton, INPUT);
-
-  begin = alarmOn[0] * 100000 + alarmOn[1] * 1000 + alarmOn[2] * 100;
-  final = alarmOff[0] * 100000 + alarmOff[1] * 1000 + alarmOff[2] * 100;
+  Serial.begin(9600);           // define serial port 9600
+  pinMode(pinLed, OUTPUT);      // define pinLed as output
+  pinMode(pinLedNano, OUTPUT);  // define pinLedNano as output
+  pinMode(pinButton, INPUT);    // define pinButton as input
+  begin = alarmOn[0] * 100000 + alarmOn[1] * 1000 + alarmOn[2] * 100;   // prepare alarm variable on
+  end = alarmOff[0] * 100000 + alarmOff[1] * 1000 + alarmOff[2] * 100;  // prepare alarm variable off
 }
 
 void loop()
 {
-  byte buttonReading = digitalRead(pinButton);
+  uint8_t buttonReading = digitalRead(pinButton); // read button state
 
-  if (buttonReading != buttonReading_old) {
+  // detected in state button on actived
+  if (buttonReading != buttonOldReading) {
     if (buttonReading == 0) {
-      blink13();
+      blink13(); // call to the blink13() function
     }
   }
 
-  buttonReading_old = buttonReading;
+  buttonOldReading = buttonReading;
 
-  if (millis() >= currentTimeA + 1000) {
+  // check if a second has passed
+  if (millis() - currentTimeA >= 1000) {
     currentTimeA = millis();
-    time();
+    time(); // call to the time() function
   }
 
+  // prepare time variable now
   timeNow = hour * 100000 + minute * 1000 + second * 100;
 
-  bool minimum = timeNow >= begin;
-  bool maximum = timeNow <= final;
+  bool minimum = timeNow >= begin;  // time now is greater than or equal to start
+  bool maximum = timeNow <= end;    // time now is less than or equal to end
 
+  // check if the two variable meet true
   if ( minimum && maximum ) {
-    if (millis() >= currentTimeB + 100) {
+    if (millis() - currentTimeB >= 100) {
       currentTimeB = millis();
-      blink();
+      blink(); // call to the blink() function
     }
   } else {
     digitalWrite(pinLed, 0);
   }
 }
 
+/*!
+  @brief   Reading the state of the 'pinLed' led and returns the status as high/low
+  @return  Returns the status of the led
+*/ 
 void blink()
 {
-  byte ledReading = digitalRead(pinLed);
+  uint8_t ledReading = digitalRead(pinLed);
   digitalWrite(pinLed, !ledReading);
 }
 
+/*!
+  @brief   Reading the state of the 'pinLedNano' led and returns the status as high/low
+  @return  Returns the status of the led
+*/ 
 void blink13()
 {
-  byte ledNanoReading = digitalRead(pinLedNano);
+  uint8_t ledNanoReading = digitalRead(pinLedNano);
   digitalWrite(pinLedNano, !ledNanoReading);
 }
 
+/*!
+  @brief   Prepare the time and data string to print on the Serial
+  @return  Returns the time in the Serial
+*/ 
 void time()
 {
   second++;
@@ -98,14 +109,15 @@ void time()
   if (hour == 24) {
     hour == 0;
   }
-  
-  String showHour = String(hour);
-  String showMinute = String(minute);
-  String showSecond = String(second);
-  
-  if (hour < 10) showHour = "0" + String(hour);
-  if (minute < 10) showMinute = "0" + String(minute);
-  if (second < 10) showSecond = "0" + String(second);
-  
-  Serial.println(showHour + ":" + showMinute + ":" + showSecond);
+
+  Serial.print("Clock: ");
+  if (hour < 10) Serial.print("0");
+  Serial.print(hour);
+  Serial.print(":");
+  if (minute < 10) Serial.print("0");
+  Serial.print(minute);
+  Serial.print(":");
+  if (second < 10) Serial.print("0");
+  Serial.print(second);
+  Serial.print("\n");
 }
